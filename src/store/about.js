1fdd2101373335d1d3dd9dae74de29af5d9052b2
minji-2219 <aabb0219@naver.com>
2023-04-29 22:06:02 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+import {Store} from '../core/heropy'
+
+
+export default new Store({
+  photo:'../img/민지 일러.jpg',
+  name:'ming / SeoMinJi',
+  email:'aabb0219@naver.com',
+  blog:'https://blog.naver.com/aabb0219',
+  github:'https://github.com/minji2219',
+  repository:'https://github.com/minji2219/Movie-App'
+})
---
+++ src/store/movie.js
@@ -0,0 +1,47 @@
+import {Store} from '../core/heropy'
+
+const store = new Store({
+  searchText : '',
+  page:1,
+  movies:[],
+  movie:{},
+  pageMax:1,
+  loading:false,
+  message:'Search for the movie title!'
+})
+
+export default store
+export const searchMovies = async page => {
+  store.state.loading=true
+  store.state.page = page
+  if (page===1){
+    store.state.movies = []
+    store.state.message=''
+  }
+ try{
+  const res= await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`)
+  const { Search,totalResults,Response,Error } = await res.json()
+  if(Response==='True'){
+  store.state.movies = [
+    ...store.state.movies,
+    ...Search
+   ]//1p + 2p 합쳐져서 다시 movies의 정보에 들어가짐(누적가능)
+   store.state.pageMax = Math.ceil(Number(totalResults)/10)
+ }else{
+  store.state.message = Error
+ }
+ }catch(error){
+  console.log('searchmovies eror:',error)
+ }finally{
+  store.state.loading = false
+ }
+} 
+
+export const getMovieDetails = async id =>{
+  try{
+    const res = await fetch(`https://omdbapi.com?apikey=7035c60c&i=${id}&plot=full`)
+    store.state.movie = await res.json()
+  }catch(error){
+    console.log('getMovieDetails error:',error)
+  }
+}
