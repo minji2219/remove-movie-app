fd8d2fec0f18b21db9430ad048335cd91e9f3ffe
minji-2219 <aabb0219@naver.com>
2023-04-30 16:07:39 +0900

    오류 수정

--- src/store/about.js
+++ src/store/about.js
@@ -1,8 +1,8 @@
 import {Store} from '../core/heropy'
-
+import MyImage from '../img/민지 일러.jpg'; 
 
 export default new Store({
-  photo:'../img/민지 일러.jpg',
+  photo:MyImage,
   name:'ming / SeoMinJi',
   email:'aabb0219@naver.com',
   blog:'https://blog.naver.com/aabb0219',
